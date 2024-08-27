dbd694c5683cdfd6914cc3d115cedf787f4ad165
eng-akash <131940138+eng-akash@users.noreply.github.com>
2024-08-27 23:15:34 +0530

    Add files via upload

---
+++ FortuneTaller.js
@@ -0,0 +1,11 @@
+var numKids  = 5;
+var partner  = 'David Beckham';
+var geoLocation = 'Costa Rica';
+var jobTitle = 'web developer';
+
+// concatenation
+
+var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ', and married to ' +
+   partner + ' ' + ' with ' + numKids + ' kids.';
+//    output
+console.log(future);
