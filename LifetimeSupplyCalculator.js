f7167b4d4a47723e5807e8f104d4a140730ebe50
eng-akash <131940138+eng-akash@users.noreply.github.com>
2024-08-27 23:27:29 +0530

    Add files via upload

---
+++ LifetimeSupplyCalculator.js
@@ -0,0 +1,6 @@
+var age = 28;
+var maxAge = 100;
+var numPerDay = 2;
+var totalNeeded = (numPerDay * 365) * (maxAge - age);
+var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+console.log(message);
